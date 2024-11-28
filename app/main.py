96ec4a788442e1c7e4ae6b27383f3443be4536bb
deltaforce26 <ahron052715@gmail.com>
2024-11-28 17:05:18 +0200

    main update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from routes.phone_blueprint import phone_bp
 
 app = Flask(__name__)
 
 
+
+
+app.register_blueprint(phone_bp)
 
 if __name__ == '__main__':
     app.run(debug=True,
